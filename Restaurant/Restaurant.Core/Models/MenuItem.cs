e7f796c84b5bdf0a261cf4234d3231af48264c6e
he1l0world <dawson.chen97@gmail.com>
2025-04-14 03:16:34 -0400

    initial change for restaurant order

---
+++ Restaurant/Restaurant.Core/Models/MenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class MenuItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string Category { get; set; }
+}
